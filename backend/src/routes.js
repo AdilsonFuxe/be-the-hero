9a2e5312b513fb7f8c3dcc71669aa770b8c3f8b5
AdilsonFuxe <adilsonfuxe.fuxe@gmail.com>
2020-03-25 00:21:24 +0100

    Back-End Da Aplicacao

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const ongController = require('./controllers/OngController');
+const incidentController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();    
+
+
+routes.post('/sessions', SessionController.create)
+
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create)
+
+
+routes.post('/incidents', incidentController.create);
+routes.get('/incidents', incidentController.index);
+routes.delete('/incidents/:id', incidentController.delete);
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
